316ea8e474d03b5c581c802b23597fe36d6abb72
Dmitri Troskov <deman777@gmail.com>
2019-01-26 19:50:54 +0200

    add props to Clock

--- src/main/scala/com/example/Clock.scala
+++ src/main/scala/com/example/Clock.scala
@@ -2,19 +2,12 @@ package com.example
 
 import java.time.LocalDateTime
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Actor, ActorRef, Props}
 import com.example.Clock._
 
 import scala.concurrent.duration.Duration.Zero
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
-
-object Clock {
-  final case class Start(time: LocalDateTime)
-  final case class Tick(time: LocalDateTime)
-  private final case object InnerTick
-  private def speedy(duration: FiniteDuration): FiniteDuration = duration * (14.minutes / 7.days).longValue()
-}
 
 class Clock(listeners: ActorRef*) extends Actor {
 
@@ -31,4 +24,12 @@ class Clock(listeners: ActorRef*) extends Actor {
       listeners.foreach(_ ! Tick(next))
       context.become(started(next.plusMinutes(1)))
   }
+}
+
+object Clock {
+  final case class Start(time: LocalDateTime)
+  final case class Tick(time: LocalDateTime)
+  private final case object InnerTick
+  private def speedy(duration: FiniteDuration): FiniteDuration = duration * (14.minutes / 7.days).longValue()
+  def props(listeners: ActorRef*): Props = Props(new Clock(listeners: _*))
 }

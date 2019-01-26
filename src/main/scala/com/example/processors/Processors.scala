c74a57282831b76ce689654001aa980ace22f1ac
Dmitri Troskov <deman777@gmail.com>
2019-01-26 23:51:53 +0200

    format code

--- src/main/scala/com/example/processors/Processors.scala
+++ src/main/scala/com/example/processors/Processors.scala
@@ -9,6 +9,7 @@ class Processors extends Actor {
     context.system.actorOf(People.props, "people")
     context.system.actorOf(Turbines.props, "turbines")
   }
+
   override def receive: Receive = {
     case event: Event => context.children.foreach(_ ! event)
   }

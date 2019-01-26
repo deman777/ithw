2c5f8aec940eebe1b5d953b70908ed37558ddef9
Dmitri Troskov <deman777@gmail.com>
2019-01-26 19:50:34 +0200

    clean code in EventsEmitter.scala

--- src/main/scala/com/example/EventsEmitter.scala
+++ src/main/scala/com/example/EventsEmitter.scala
@@ -3,14 +3,14 @@ package com.example
 import akka.actor.{Actor, ActorRef, Props}
 import com.example.Clock.Tick
 
-class EventsEmitter(events: Seq[Event], processor: ActorRef) extends Actor {
+class EventsEmitter(events: Seq[Event], processors: ActorRef) extends Actor {
   override def receive: Receive = withEvents(events)
 
   private def withEvents(events: Seq[Event]): Receive = {
-    case Tick(time) =>
-      val (now, next) = events.span(_.timestamp.compareTo(time) <= 0)
-      now.foreach(processor ! _)
-      context.become(withEvents(next))
+    case Tick(now) =>
+      val (nowEvents, futureEvents) = events.span(_.timestamp.compareTo(now) <= 0)
+      nowEvents.foreach(processors ! _)
+      context.become(withEvents(futureEvents))
   }
 }
 

ec367af631c717b8008ac45bf026908a0dd62bf2
Dmitri Troskov <deman777@gmail.com>
2019-01-26 18:01:59 +0200

    add props to EventsEmitter and rename some variables

--- src/main/scala/com/example/EventsEmitter.scala
+++ src/main/scala/com/example/EventsEmitter.scala
@@ -1,15 +1,20 @@
 package com.example
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Actor, ActorRef, Props}
 import com.example.Clock.Tick
 
 class EventsEmitter(events: Seq[Event], processor: ActorRef) extends Actor {
   override def receive: Receive = withEvents(events)
 
-  private def withEvents(movements: Seq[Event]): Receive = {
+  private def withEvents(events: Seq[Event]): Receive = {
     case Tick(time) =>
-      val (now, next) = movements.span(_.timestamp.compareTo(time) <= 0)
+      val (now, next) = events.span(_.timestamp.compareTo(time) <= 0)
       now.foreach(processor ! _)
       context.become(withEvents(next))
   }
+}
+
+object EventsEmitter {
+  def props(events: Seq[Event], processor: ActorRef): Props =
+    Props(new EventsEmitter(events, processor))
 }

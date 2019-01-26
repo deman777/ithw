efcfc5f6d785ea4eb248e943be9bcf4deab0e193
Dmitri Troskov <deman777@gmail.com>
2019-01-26 19:01:38 +0200

    add Error class and log it in printer

--- src/main/scala/com/example/Printer.scala
+++ src/main/scala/com/example/Printer.scala
@@ -1,10 +1,40 @@
 package com.example
 
-import akka.actor.{Actor, ActorLogging}
+import java.time.LocalDateTime
+
+import akka.actor.{Actor, ActorLogging, Props}
+import com.example.Printer.Error
 
 class Printer extends Actor with ActorLogging {
   override def receive: Receive = {
-    case message =>
-      log.info(message.toString)
+    case error: Error =>
+      log.info(error.toString)
+  }
+}
+
+object Printer {
+  val props: Props = Props[Printer]
+  private case class ErrorState(name: String) {
+    override def toString: String = name
+  }
+  case object Open extends ErrorState("open")
+  case object Closed extends ErrorState("closed")
+  case class Error(
+    date: LocalDateTime,
+    turbine: TurbineId,
+    person: PersonId,
+    error: String,
+    errorState: ErrorState
+  ) {
+    def toJson: String =
+      s"""
+         |{
+         |  "date": "$date"
+         |  "turbine": "${turbine.id}",
+         |  "person": "${person.id}",
+         |  "error": "$error",
+         |  "error_state": "$errorState",
+         |}
+      """.stripMargin
   }
 }

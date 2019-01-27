a987d5bf5ea6e5d30462922964eda4cff69fed01
Dmitri Troskov <deman777@gmail.com>
2019-01-28 00:26:23 +0200

    refactor Emitters.scala a bit

--- src/main/scala/com/example/emitters/Emitters.scala
+++ src/main/scala/com/example/emitters/Emitters.scala
@@ -7,19 +7,23 @@ import com.example.Clock.{Start, Tick}
 import com.example.Event
 import com.example.emitters.Emitters.Read
 
-import scala.PartialFunction.empty
 import scala.collection.immutable.Set
 
 class Emitters extends Actor {
 
-  override def receive: Receive = empty
+  override def receive: Receive = reading(Set(
+    context.actorOf(MovementsEmitter.props, "movements"),
+    context.actorOf(TurbineStatusUpdatesEmitter.props, "turbines")
+  ), LocalDateTime.MAX)
 
-  override def preStart(): Unit = {
-    val refs = Set(
-      context.actorOf(MovementsEmitter.props, "movements"),
-      context.actorOf(TurbineStatusUpdatesEmitter.props, "turbines")
-    )
-    context.become(reading(refs, LocalDateTime.MAX))
+  private def reading(refs: Set[ActorRef], minTimestamp: LocalDateTime): Receive = {
+    case read: Read =>
+      onRead(refs - sender(), min(minTimestamp, read.minTimestamp))
+  }
+
+  private val emitting: Receive = {
+    case tick: Tick => context.children.foreach(_.forward(tick))
+    case event: Event => context.parent.forward(event)
   }
 
   def onRead(refs: Set[ActorRef], minTimestamp: LocalDateTime): Unit = {
@@ -30,16 +34,6 @@ class Emitters extends Actor {
     else {
       context.become(reading(refs, minTimestamp))
     }
-  }
-
-  def reading(refs: Set[ActorRef], minTimestamp: LocalDateTime): Receive = {
-    case read: Read =>
-      onRead(refs - sender(), min(minTimestamp, read.minTimestamp))
-  }
-
-  private val emitting: Receive = {
-    case tick: Tick => context.children.foreach(_.forward(tick))
-    case event: Event => context.parent.forward(event)
   }
 
   private def min(a: LocalDateTime, b: LocalDateTime) = if (a.compareTo(b) < 0) a else b

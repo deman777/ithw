48a00d5897e6dfc5fdab01e819300c7798ae341c
Dmitri Troskov <deman777@gmail.com>
2019-01-28 00:04:15 +0200

    pass errors from people to processors

--- src/main/scala/com/example/processors/People.scala
+++ src/main/scala/com/example/processors/People.scala
@@ -1,12 +1,13 @@
 package com.example.processors
 
 import akka.actor.{Actor, Props}
-import com.example.{PersonId, Movement}
+import com.example.{LogError, Movement, PersonId}
 
 class People extends Actor {
 
   override def receive: Receive = {
     case m@Movement(_, _, personId: PersonId, _) => person(personId) ! m
+    case logError: LogError => context.parent.forward(logError)
   }
 
   private def person(personId: PersonId) =

0588771459760ed3eb237c80f31c834763232ef4
Dmitri Troskov <deman777@gmail.com>
2019-01-28 02:15:56 +0200

    do not fail Person on not handled cases

--- src/main/scala/com/example/processors/Person.scala
+++ src/main/scala/com/example/processors/Person.scala
@@ -13,20 +13,24 @@ class Person(personId: PersonId) extends Actor {
 
   def after(last: Movement): Receive = {
     case movement: Movement =>
+      context.become(after(movement))
       last match {
         case Movement(_, _: VesselId, _, Enter) =>
           movement match {
             case Movement(timestamp, turbineId: TurbineId, _, Enter) =>
               error(timestamp, turbineId, "Can not enter turbine without leaving ship")
+            case _ =>
           }
         case Movement(_, _: VesselId, _, Exit) =>
           movement match {
             case Movement(timestamp, turbineId: TurbineId, _, Exit) =>
               error(timestamp, turbineId, "Can not exit turbine without entering it")
+            case _ =>
           }
+        case _ =>
       }
-      context.become(after(movement))
   }
+
   private def error(timestamp: LocalDateTime, turbineId: TurbineId, message: String): Unit = {
     context.parent ! LogError(timestamp, turbineId, Some(personId), message, Closed)
   }
